39af36eccdf4fce4541842ae7f7982ab679939d9
Coleman Imhoff <colemanimhoff@gmail.com>
2018-05-30 15:54:47 -0600

    added back of beer card and more styling

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const getUrl = 'https://api.punkapi.com/v2/beers'
+const getUrl = 'https://api.punkapi.com/v2/beers/?page=2&per_page=20'
 
 getData(getUrl)
 
@@ -9,17 +9,40 @@ function getData (url) {
             createBeerCard(beers)
             console.log(beers)
         })
+        .then(beers => flipCard(beers))
 }
 
 function createBeerCard(beers) {
     const beerContainer = document.querySelector('main')
     return beers.forEach(beer => {
         beerContainer.innerHTML += (`
-                <div class="front">
-                    <h2>${beer.name}</h2>
-                    <img src=${beer.image_url}></img>
-                    <p>${beer.tagline}</p>
+                <div class="beer-card">
+                    <div class="front">
+                        <h2>${beer.name}</h2>
+                        <img src=${beer.image_url}></img>
+                        <p>${beer.tagline}</p>
+                    </div>
+                    <div class="back">
+                        <h3>First Brewed</h3>
+                        <p>${beer.first_brewed}</p>
+                        <h3>ABV</h3>
+                        <p>${beer.abv}%</p>
+                        <h3>Food Pairing</h3>
+                        <p>${beer.food_pairing[0]}</p>
+                    </div>
                 </div>
         `)
+    })
+    return beerContainer
+}
+
+function flipCard (beers) {
+    const beerCards = document.querySelectorAll('.beer-card')
+    beerCards.forEach(beer => {
+        beer.addEventListener('click', (event) => {
+            console.log(beer.firstElementChild)
+            beer.firstElementChild.classList.toggle('clicked')
+            beer.firstElementChild.classList.remove('front')
+        })
     })
 }
